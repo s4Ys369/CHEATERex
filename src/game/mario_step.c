f7737a0816793cc30de28fad36d5bbdee66b195d
TheGag96 <thegag96@gmail.com>
2020-10-03 17:36:06 -0500

    Allow spin jump fall to be slowed when holding A with wing cap

--- src/game/mario_step.c
+++ src/game/mario_step.c
@@ -547,9 +547,21 @@ void apply_gravity(struct MarioState *m) {
             m->vel[1] = -75.0f;
         }
     } else if (m->action == ACT_SPIN_JUMP) {
-        m->vel[1] -= (m->vel[1] > 0.0f) ? 4.0f : 1.4f;
-        if (m->vel[1] < -75.0f) {
-            m->vel[1] = -75.5f;
+        if ((m->flags & MARIO_WING_CAP) && m->vel[1] < 0.0f && (m->input & INPUT_A_DOWN)) {
+            m->marioBodyState->wingFlutter = TRUE;
+            m->vel[1] -= 0.7f;
+            if (m->vel[1] < -37.5f) {
+                if ((m->vel[1] += 1.4f) > -37.5f) {
+                    m->vel[1] = -37.5f;
+                }
+            }
+        }
+        else
+        {
+            m->vel[1] -= (m->vel[1] > 0.0f) ? 4.0f : 1.4f;
+            if (m->vel[1] < -75.0f) {
+                m->vel[1] = -75.5f;
+            }
         }
     } else if (m->action == ACT_WALL_SLIDE) {
         m->vel[1] -= 2.0f;
